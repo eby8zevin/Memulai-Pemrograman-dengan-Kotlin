e186128788e0e0b0376baf9f18c560653bea70c6
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-07 20:49:51 +0700

    If Expression: If Else If

--- Kotlin Fundamental/If Expression/If Else If/src/App.kt
+++ Kotlin Fundamental/If Expression/If Else If/src/App.kt
@@ -4,7 +4,7 @@ fun main() {
     val now = 7
     val office: String
     office = if (now > 7) {
-        "Office already ope"
+        "Office already open"
     } else if (now == officeOpen){
         "Wait a minute, office will be open"
     } else {
