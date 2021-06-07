029aa63d41f3622867b9adca94279e4871dbeef3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-07 20:47:55 +0700

    If Expression: If as a Expression

--- Kotlin Fundamental/If Expression/If as a Expression/src/App.kt
+++ Kotlin Fundamental/If Expression/If as a Expression/src/App.kt
@@ -4,7 +4,7 @@ fun main() {
     val now = 20
     val office: String
     office = if (now > officeOpen) {
-        "Office already open"
+        "Office already ope"
     } else {
         "Office is closed"
     }
