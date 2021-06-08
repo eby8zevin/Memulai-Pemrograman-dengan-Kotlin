a3007a955a64fc0b3802d1848cd96ad9bbbb51f6
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:14:25 +0700

    When Expression: When with Range as a Condition

--- Control Flow/When Expression/When with Range as a Condition/src/App.kt
+++ Control Flow/When Expression/When with Range as a Condition/src/App.kt
@@ -4,7 +4,7 @@ fun main() {
     val ranges = 10..50
 
     when(value){
-        in ranges -> println("value is in the range")
+        in ranges -> println("valu is in the range")
         !in ranges -> println("value is outside the range")
         else -> println("value undefined")
     }
