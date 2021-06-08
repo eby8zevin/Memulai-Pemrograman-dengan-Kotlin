03be75f9b6042772f7422a64fa9e2039af11bf97
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:48:48 +0700

    Data Class: Comparing data class

--- Data Class dan Collections/Data Class/Comparing data class/src/App.kt
+++ Data Class dan Collections/Data Class/Comparing data class/src/App.kt
@@ -1,9 +1,9 @@
 data class DataUser(val name : String, val age : Int)
 
 fun main(){
-    val dataUser = DataUser("nrohmen", 17)
-    val dataUser2 = DataUser("nrohmen", 17)
-    val dataUser3 = DataUser("dimas", 24)
+    val dataUser = DataUser("Ahmad Abu Hasan", 23)
+    val dataUser2 = DataUser("Ahmad Abu Hasan", 23)
+    val dataUser3 = DataUser("Eby", 24)
 
     println(dataUser.equals(dataUser2))
     println(dataUser.equals(dataUser3))
