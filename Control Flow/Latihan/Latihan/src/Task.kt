6a50be4478904d30d735a67c97701fa3ff240eb7
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:40:31 +0700

    Control Flow: Latihan

--- Control Flow/Latihan/Latihan/src/Task.kt
+++ Control Flow/Latihan/Latihan/src/Task.kt
@@ -12,13 +12,13 @@ fun main() {
 
     for (number in listNumber) {
         // TODO 1
-        if (false) continue
+        if (number%2 == 0) continue
 
         // TODO 2
-        if (false) break
+        if (number > 15) break
 
         // TODO 3
-        val result =0
+        val result = number * (number + 10)
         println("range result is $result")
     }
 }
