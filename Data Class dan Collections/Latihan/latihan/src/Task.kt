805815a65975f22c03e688beca1c10023dce5c17
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 20:28:22 +0700

    Data Class dan Collections: Latihan

--- Data Class dan Collections/Latihan/latihan/src/Task.kt
+++ Data Class dan Collections/Latihan/latihan/src/Task.kt
@@ -10,14 +10,23 @@
 fun main() {
 
     // TODO 1
-    val vehicle = mapOf<String, String>()
+    val vehicle = mapOf<String, String>(
+        "type" to "Motorcycle",
+        "maxSpeed" to "230Km/s",
+        "maxTank" to "10Ltr"
+    )
 
     // TODO 2
-    val type = ""
-    val maxSpeed = ""
-    val maxTank = ""
+    val type = vehicle.getValue("type")
+    val maxSpeed = vehicle.getValue("maxSpeed")
+    val maxTank = vehicle.getValue("maxTank")
 
     // TODO 3
-    println()
+    println("""
+        Vehicle
+        Type: $type
+        Maximal Speed: $maxSpeed
+        Maximal Tank: $maxTank
+    """.trimIndent())
 
 }
