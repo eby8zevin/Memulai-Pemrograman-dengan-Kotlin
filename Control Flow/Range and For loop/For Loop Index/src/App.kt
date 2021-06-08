9a8cb18bf5221674c7f4c97e0a9a43dc15327550
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:26:27 +0700

    Range and For loop: For Loop Index

--- Control Flow/Range and For loop/For Loop Index/src/App.kt
+++ Control Flow/Range and For loop/For Loop Index/src/App.kt
@@ -3,6 +3,6 @@ fun main() {
     val ranges = 1.rangeTo(10) step 3
 
     for ((index, value) in ranges.withIndex()) {
-        println("value $value with index $index")
+        println("valu $value with index $index")
     }
 }
