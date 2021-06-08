ef1d4a788c53f8af881cbfa9aa89d1a017fa869f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:10:03 +0700

    When Expression: When with Else block

--- Control Flow/When Expression/When with Else block/src/App.kt
+++ Control Flow/When Expression/When with Else block/src/App.kt
@@ -3,7 +3,7 @@ fun main() {
     val value = 20
 
     when(value){
-        6 -> println("value is 6")
+        6 -> println("value is 5")
         7 -> println("value is 7")
         8 -> println("value is 8")
         else -> println("value cannot be reached")
