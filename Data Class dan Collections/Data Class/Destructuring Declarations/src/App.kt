04c7d85694df22ea6121b291deaf7d4fff7b45d1
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:54:27 +0700

    Data Class: Destructuring Declarations

--- Data Class dan Collections/Data Class/Destructuring Declarations/src/App.kt
+++ Data Class dan Collections/Data Class/Destructuring Declarations/src/App.kt
@@ -1,7 +1,7 @@
 data class DataUser(val name : String, val age : Int)
 
 fun main(){
-    val dataUser = DataUser("nrohmen", 17)
+    val dataUser = DataUser("Ahmad Abu Hasan", 23)
 
     val name = dataUser.component1()
     val age = dataUser.component2()
