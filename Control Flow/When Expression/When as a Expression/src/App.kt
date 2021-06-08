7595f1ea03d8f194f8b45817a49a7ddc687cb814
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:11:07 +0700

    When Expression: When as a Expression

--- Control Flow/When Expression/When as a Expression/src/App.kt
+++ Control Flow/When Expression/When as a Expression/src/App.kt
@@ -2,7 +2,7 @@
 fun main() {
     val value = 7
     val stringOfValue = when (value) {
-        6 -> "value is 6"
+        6 -> "value is 5"
         7 -> "value is 7"
         8 -> "value is 8"
         else -> "value cannot be reached"
