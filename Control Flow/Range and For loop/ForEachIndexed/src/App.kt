4948eb7ae33e9902daf33168bd64e0e64ea026c9
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:29:54 +0700

    Range and For loop: ForEachIndexed

--- Control Flow/Range and For loop/ForEachIndexed/src/App.kt
+++ Control Flow/Range and For loop/ForEachIndexed/src/App.kt
@@ -3,6 +3,6 @@ fun main() {
     val ranges = 1.rangeTo(10) step 3
 
     ranges.forEachIndexed { index, value ->
-        println("valu $value with index $index")
+        println("value $value with index $index")
     }
 }
