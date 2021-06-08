3e4fb767e7d34d79e46de5c4cefb4913cd7d4cfe
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:46:58 +0700

    Data Class: Different data class with class

--- Data Class dan Collections/Data Class/Different data class with class/src/App.kt
+++ Data Class dan Collections/Data Class/Different data class with class/src/App.kt
@@ -3,8 +3,8 @@ class User(val name : String, val age : Int)
 data class DataUser(val name : String, val age : Int)
 
 fun main(){
-    val user = User("nrohmen", 17)
-    val dataUser = DataUser("nrohmen", 17)
+    val user = User("Ahmad Abu Hasan", 23)
+    val dataUser = DataUser("Ahmad Abu Hasan", 23)
 
     println(user)
     println(dataUser)
