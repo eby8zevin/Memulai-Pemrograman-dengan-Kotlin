efcf2ace68c1cbada911b67d4268721b5dab7100
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:55:31 +0700

    Data Class: Create variables based on data class objects

--- Data Class dan Collections/Data Class/Create variables based on data class objects/src/App.kt
+++ Data Class dan Collections/Data Class/Create variables based on data class objects/src/App.kt
@@ -1,7 +1,7 @@
 data class DataUser(val name : String, val age : Int)
 
 fun main(){
-    val dataUser = DataUser("nrohmen", 17)
+    val dataUser = DataUser("Ahmad Abu Hasan", 23)
     val (name, age) = dataUser
 
     println("My name is $name, I am $age years old")
