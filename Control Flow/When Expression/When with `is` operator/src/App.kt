f98d390f120e1cd917072debf2aaab22e034e75a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-08 19:14:02 +0700

    When Expression: When with `is` operator

--- Control Flow/When Expression/When with `is` operator/src/App.kt
+++ Control Flow/When Expression/When with `is` operator/src/App.kt
@@ -2,7 +2,7 @@
 fun main() {
     val anyType : Any = 100L
     when(anyType){
-        is Long -> println("the valu has a Long type")
+        is Long -> println("the value has a Long type")
         is String -> println("the value has a String type")
         else -> println("undefined")
     }
