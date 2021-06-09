665e01d2bfe14602c4cc93f382874a18c8b105d5
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 21:07:18 +0700

    Import dan Packages: Contoh 3

--- Object-Oriented/Import dan Packages/Contoh 3/src/com/dicoding/oop/App.kt
+++ Object-Oriented/Import dan Packages/Contoh 3/src/com/dicoding/oop/App.kt
@@ -4,7 +4,7 @@ import com.dicoding.oop.utils.*
 
 fun main() {
     sayHello()
-    println(factorial(4.1))
+    println(factorial(4.0))
     println(pow(3.0, 2.0))
     println(PI)
     println(areaOfCircle(13.0))
