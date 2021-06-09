73de76084ad720a5fa5c1a28afb3df9b4c6995d0
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 22:38:09 +0700

    Coroutine Dispatcher: Single Thread Context

--- Coroutines/Coroutine Dispatcher/Single Thread Context/src/App.kt
+++ Coroutines/Coroutine Dispatcher/Single Thread Context/src/App.kt
@@ -3,7 +3,7 @@ import kotlinx.coroutines.*
 fun main() = runBlocking<Unit> {
     val dispatcher = newSingleThreadContext("myThread")
     launch(dispatcher) {
-        println("Starting in ${Thread.currentThread().name}")
+        println("Startin in ${Thread.currentThread().name}")
         delay(1000)
         println("Resuming in ${Thread.currentThread().name}")
     }.start()
