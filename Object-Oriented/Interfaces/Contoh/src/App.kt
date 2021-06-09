ccb5af9f44a98258333381cbcd6f899c858cfdaa
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 21:36:27 +0700

    Interfaces: Contoh

--- Object-Oriented/Interfaces/Contoh/src/App.kt
+++ Object-Oriented/Interfaces/Contoh/src/App.kt
@@ -6,7 +6,7 @@ interface IFly {
 class Bird(override val numberOfWings: Int) : IFly {
     override fun fly() {
         if(numberOfWings > 0) println("Flying with $numberOfWings wings")
-        else println("I'm Flying without wing")
+        else println("I'm Flying without wings")
     }
 }
 
