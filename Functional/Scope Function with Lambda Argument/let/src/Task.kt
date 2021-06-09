6e1211de0e299e7b38c7496cdfc7b6b448a5d2fd
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 08:54:49 +0700

    Scope Function with Lambda Argument: let

--- Functional/Scope Function with Lambda Argument/let/src/Task.kt
+++ Functional/Scope Function with Lambda Argument/let/src/Task.kt
@@ -1,7 +1,7 @@
 fun main() {
     val message: String? = null
     message?.let {
-        val length = it.length * 3
+        val length = it.length * 2
         val text = "text length $length"
         println(text)
     }.run {
