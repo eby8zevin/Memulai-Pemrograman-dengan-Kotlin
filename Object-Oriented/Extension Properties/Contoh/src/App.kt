3942bc50b74766dcb078785e2bcbc5443abb9bb4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 21:31:24 +0700

    Extension Properties: Contoh

--- Object-Oriented/Extension Properties/Contoh/src/App.kt
+++ Object-Oriented/Extension Properties/Contoh/src/App.kt
@@ -1,7 +1,7 @@
 class Animal(var name: String, var weight: Double, var age: Int, var isMammal: Boolean)
 
 val Animal.getAnimalInfo : String
-    get() =  "Nam: ${this.name}, Berat: ${this.weight}, Umur: ${this.age} Mamalia: ${this.isMammal}"
+    get() =  "Nama: ${this.name}, Berat: ${this.weight}, Umur: ${this.age} Mamalia: ${this.isMammal}"
 
 fun main() {
     val dicodingCat = Animal("Dicoding Miaw", 5.0, 2, true)
