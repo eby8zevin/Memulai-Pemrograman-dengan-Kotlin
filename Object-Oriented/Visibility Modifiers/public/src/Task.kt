9f73da23c7fe137f3cfacc4eb156a1f913420c71
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 20:53:26 +0700

    Visibility Modifiers: public

--- Object-Oriented/Visibility Modifiers/public/src/Task.kt
+++ Object-Oriented/Visibility Modifiers/public/src/Task.kt
@@ -1,7 +1,7 @@
 class Animal(val name: String, val weight: Double, val age: Int, val isMammal: Boolean = true)
 
 fun main() {
-    val dicodingCat = Animal("Dicoding Miaw", 2.2, 2)
+    val dicodingCat = Animal("Dicodin Miaw", 2.2, 2)
     dicodingCat.name
     dicodingCat.weight
     dicodingCat.age
