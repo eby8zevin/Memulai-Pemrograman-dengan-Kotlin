de24b83bb33dbebce7d8ac30865597fb394fc802
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 08:51:32 +0700

    Scope Function with Lambda Receiver: with

--- Functional/Scope Function with Lambda Receiver/with/src/Task.kt
+++ Functional/Scope Function with Lambda Receiver/with/src/Task.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val message = "Hello Kotlin!"
+    val message = "Hello Kotlin"
     val result = with(message) {
         "First character is ${this[0]}" +
                 " and last character is ${this[this.length - 1]}"
