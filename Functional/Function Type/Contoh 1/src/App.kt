7b0102f50fee26379bc4e10591ddbc25ca14b490
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 08:20:25 +0700

    Function Type: Contoh 1

--- Functional/Function Type/Contoh 1/src/App.kt
+++ Functional/Function Type/Contoh 1/src/App.kt
@@ -3,7 +3,7 @@ val sum: (Int, Int) -> Int = { valueA, valueB -> valueA + valueB }
 val multiply: (Int, Int) -> Int = { valueA, valueB -> valueA * valueB }
 
 fun main() {
-    val sumResult = sum.invoke(10, 11)
+    val sumResult = sum.invoke(10, 10)
     val multiplyResult = multiply.invoke(20, 20)
 
     println(sumResult)
