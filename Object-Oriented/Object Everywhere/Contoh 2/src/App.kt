88bc6a7d7285f656c9e3f07cc0ec925981f74c9f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 20:33:26 +0700

    Object Everywhere: Contoh 2

--- Object-Oriented/Object Everywhere/Contoh 2/src/App.kt
+++ Object-Oriented/Object Everywhere/Contoh 2/src/App.kt
@@ -1,6 +1,6 @@
 // main function
 fun main() {
-    val someString = "123"
+    val someString = "1234"
     val someInt = someString.toInt()
     val someOtherString = "12.34"
     val someDouble = someOtherString.toDouble()
