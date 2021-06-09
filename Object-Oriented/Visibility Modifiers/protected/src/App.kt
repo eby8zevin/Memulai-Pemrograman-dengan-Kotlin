5c0cd5af0a849eeb1082446018893c2897a59ccf
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 20:57:19 +0700

    Visibility Modifiers: protected

--- Object-Oriented/Visibility Modifiers/protected/src/App.kt
+++ Object-Oriented/Visibility Modifiers/protected/src/App.kt
@@ -4,7 +4,7 @@ class Cat(pName: String, pWeight: Double) : Animal(pName, pWeight)
 
 
 fun main() {
-    val cat = Cat("Dicoding Miaw", 2.0)
+    val cat = Cat("Dicodin Miaw", 2.0)
     println("Nama Kucing: ${cat.name}")
     // println("Berat Kucing: ${cat.weight}") <- Cannot access 'weight': it is protected in 'Cat'
 }
