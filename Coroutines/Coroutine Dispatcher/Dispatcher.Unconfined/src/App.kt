e7f7fa5353a10ec7cbcab7056359f360a4ed3a14
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 22:34:58 +0700

    Coroutine Dispatcher: Dispatcher.Unconfined

--- Coroutines/Coroutine Dispatcher/Dispatcher.Unconfined/src/App.kt
+++ Coroutines/Coroutine Dispatcher/Dispatcher.Unconfined/src/App.kt
@@ -3,7 +3,7 @@ import kotlinx.coroutines.*
 @ExperimentalCoroutinesApi
 fun main() = runBlocking<Unit> {
     launch(Dispatchers.Unconfined) {
-        println("Starting in ${Thread.currentThread().name}")
+        println("Startin in ${Thread.currentThread().name}")
         delay(1000)
         println("Resuming in ${Thread.currentThread().name}")
     }.start()
