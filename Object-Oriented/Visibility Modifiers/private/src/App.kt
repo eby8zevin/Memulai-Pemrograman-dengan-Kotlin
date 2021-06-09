105bf9b3ef2e278643821063e1a5e6805cf0d9f7
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 20:54:31 +0700

    Visibility Modifiers: private

--- Object-Oriented/Visibility Modifiers/private/src/App.kt
+++ Object-Oriented/Visibility Modifiers/private/src/App.kt
@@ -8,7 +8,7 @@ class Animal(private var name: String, private val weight: Double, private val a
 }
 
 fun main() {
-    val dicodingCat = Animal("Dicoding Miaw", 2.5, 2)
+    val dicodingCat = Animal("Dicodin Miaw", 2.5, 2)
 
     // dicoding.name <- Cannot access '[PROPERTY]': it is private in 'Animal'
     println(dicodingCat.getName())
