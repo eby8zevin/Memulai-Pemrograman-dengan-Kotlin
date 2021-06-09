2c3082ae9711cd5c096c060f3aa288b580b51c40
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 20:31:27 +0700

    Object Everywhere: Contoh 1

--- Object-Oriented/Object Everywhere/Contoh 1/src/App.kt
+++ Object-Oriented/Object Everywhere/Contoh 1/src/App.kt
@@ -1,6 +1,6 @@
 // main function
 fun main() {
-    val company = "Dicoding"
+    val company = "Dicodin"
     println(company.reversed())
     println(company.toUpperCase())
     println(company.toLowerCase())
