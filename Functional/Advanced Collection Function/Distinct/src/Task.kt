0824a3a8ec569692369f4688a493d19f9b935f7e
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 10:00:59 +0700

    Advanced Collection Function: Distinct

--- Functional/Advanced Collection Function/Distinct/src/Task.kt
+++ Functional/Advanced Collection Function/Distinct/src/Task.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val total = listOf(1, 2, 1, 3, 4, 5, 2, 3, 4, 5)
+    val total = listOf(1, 2, 1, 3, 4, 5, 2, 3, 4, 6)
     val distinct = total.distinct()
 
     println(distinct)
