384aab32e12150137c9e5a0a1c615f45b91b7142
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 09:34:47 +0700

    Function Inside Function: Inner function

--- Functional/Function Inside Function/Inner function/src/Task.kt
+++ Functional/Function Inside Function/Inner function/src/Task.kt
@@ -1,7 +1,7 @@
 fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
     fun Int.validateNumber(){
         if (this == 0) {
-            throw IllegalArgumentException("valu must be better than 0")
+            throw IllegalArgumentException("value must be better than 0")
         }
     }
 
