ae7fb41bdaf244e7a9512cd5e937cf1eb43c7f2d
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-09 08:33:53 +0700

    Library Helper: with

--- Functional/Library Helper/with/src/App.kt
+++ Functional/Library Helper/with/src/App.kt
@@ -1,6 +1,6 @@
 // main function
 fun main() {
-    val message = "Hello Kotlin"
+    val message = "Hello Kotlin!"
     val result = with(message) {
         "First character is ${this[0]}" +
                 " and last character is ${this[this.length - 1]}"
